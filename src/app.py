0799483b01907a7ac7a71cdc4d28a666fed1048e
Bloctans <93960460+Bloctans@users.noreply.github.com>
2022-08-10 09:54:05 -0500

    Yaaay time to be under more stress
    (and peer pressure)

---
+++ src/app.py
@@ -0,0 +1,59 @@
+from flask import Flask
+import os
+
+"""
+    todo:
+        when released to github:
+            add reindexing
+            optimizations
+"""
+
+from pymongo import MongoClient
+
+mongo_ip = "mongodb://localhost:27017"
+db = MongoClient(mongo_ip)["indexedfiles"]
+
+for item in ["html"]:
+    if not item in db.list_collection_names():
+        print("Creating collection {0}".format(item))
+        db.create_collection(name=item)
+
+db["html"].create_index("html")
+
+app = Flask(__name__)
+
+@app.route("/api/mod/runindexer")
+def runindexer():
+    return "a"
+
+"""@app.route("/api/mod/reindex/<site>")
+def reindexsite():
+    return "remove from mongo and re-fetch all indexes from that""""
+
+@app.route("/api/mod/dropindexes")
+def dropindexes():
+    return "remove all indexes"
+
+@app.route("/api/getresults/<query>")
+def getsearch(query):
+    return query
+
+@app.route("/api/getheaders/<site>/")
+def getheaders(site):
+    return "headers query" + site
+
+@app.route("/")
+def root():
+    return "<body><script>location.replace('./api/')</script></body>"
+
+@app.route("/api/")
+def apidoc():
+    return """<pre>
+    The Api Seems to be up!
+        The endpoints are:
+            /api/getresults/<query>, Returns JSON
+            /api/status, Returns JSON
+            /api/mod/runindexer, Returns Error or string, Headers: modkey, baseurl and time
+            /api/mod/dropindexes, Returns StatusCode
+            /api/getheaders/<site>, Returns JSON
+    </pre>"""
